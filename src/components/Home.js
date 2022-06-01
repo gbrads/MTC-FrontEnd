83f1be70473018eae386e719af1668a12e90004c
boost <garret818@gmail.com>
2022-05-31 23:29:48 -0700

    Image Fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ function Home() {
     <div>
       <section className="Home" id="Home" style={{ backgroundImage: "", backgroundPosition: "center", backgroundSize: "cover", backgroundRepeat: "no-repeat" }}>
         <div>
-          <img className="logo" src="/images/main-logo.png" />
+          <img className="logo" src="../images/main-logo.png" />
         </div>
       </section>
     </div>
