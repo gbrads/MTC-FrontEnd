265008e2c4342fe7a174707c007bc984cb72dcdb
boost <garret818@gmail.com>
2022-06-02 00:28:05 -0700

    Update Home Image

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 function Home() {
   return (
     <div>
-      <section className="Home" id="Home" style={{ backgroundImage: "url(/images/bg1.png)", backgroundPosition: "center", backgroundSize: "cover", backgroundRepeat: "no-repeat" }}>
+      <section className="Home" id="Home" style={{ backgroundImage: "url(/Images/bg1.png)", backgroundPosition: "center", backgroundSize: "cover", backgroundRepeat: "no-repeat" }}>
       </section>
     </div>
   );
